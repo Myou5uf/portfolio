caebfbcbe7239f19d7e5d0820c57e4151ab68e5d
Myou5uf <myxamed12l@gmail.com>
2022-03-26 00:19:17 +0700

    Added the number of days to the timer and adjusted the number of hours (no more than 24)

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
 const timerBlock = document.querySelector(".timer__time"); // блок таймера
-const deadLine = "24 march 2022"; // дедлайн
+const deadLine = "31 march 2022"; // дедлайн
 
 let interval; // Идентификатор интервала
 
@@ -9,17 +9,19 @@ const updateClock = () => {
     const dateDeadLine = new Date(deadLine).getTime(); // кол-во секунд до дедлайна
     const timeRemaning = (dateDeadLine - date) / 1000; // Кол-во оставшихся мс
 
-    const hours = Math.floor(timeRemaning / 60 / 60); // Кол-во оставшихся часов
+    const days = Math.floor(timeRemaning / 60 / 60 / 24); // Кол-во оставшихся дней
+    const hours = Math.floor((timeRemaning / 60 / 60) % 24); //  часов
     const minutes = Math.floor((timeRemaning / 60) % 60); // минут
     const seconds = Math.floor(timeRemaning % 60); // секунд
 
     // Добавляем нолик в начало, если часы, минуты или секунды меньше 10
-    const fHours = hours < 10 ? "0" + hours : hours;
+    let fDays = days < 10 ? "0" + days : days;
+    let fHours = hours < 10 ? "0" + hours : hours;
     const fMinutes = minutes < 10 ? "0" + minutes : minutes;
     const fSeconds = seconds < 10 ? "0" + seconds : seconds;
     // console.log();
     // меняем текстовое содержимое блока timerBlock
-    timerBlock.textContent = `${fHours}:${fMinutes}:${fSeconds}`; // задаем текущее для таймера
+    timerBlock.textContent = `${fDays}:${fHours}:${fMinutes}:${fSeconds}`; // задаем текущее для таймера
 
     // Если времени не осталось
     if (timeRemaning < 0) {
